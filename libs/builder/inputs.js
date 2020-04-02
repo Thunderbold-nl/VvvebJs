492c64ef8e98a5829064c05e7fca62f7868e3b12
Theo <61891191+Thunderbold-nl@users.noreply.github.com>
2020-04-02 21:32:25 +0200

    Update inputs.js

--- libs/builder/inputs.js
+++ libs/builder/inputs.js
@@ -1,18 +1,14 @@
 /*
 Copyright 2017 Ziadin Givan
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
    http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 https://github.com/givanz/VvvebJs
 */
 
@@ -64,6 +60,54 @@ var TextInput = $.extend({}, Input, {
 	
 	init: function(data) {
 		return this.render("textinput", data);
+	},
+  }
+);
+
+var TelInput = $.extend({}, Input, {
+
+    events: [
+        ["blur", "onChange", "input"],
+	 ],
+
+	init: function(data) {
+		return this.render("telinput", data);
+	},
+  }
+);
+
+var EmailInput = $.extend({}, Input, {
+
+    events: [
+        ["blur", "onChange", "input"],
+	 ],
+
+	init: function(data) {
+		return this.render("emailinput", data);
+	},
+  }
+);
+
+var DateInput = $.extend({}, Input, {
+
+    events: [
+        ["blur", "onChange", "input"],
+	 ],
+
+	init: function(data) {
+		return this.render("dateinput", data);
+	},
+  }
+);
+
+var TimeInput = $.extend({}, Input, {
+
+    events: [
+        ["blur", "onChange", "input"],
+	 ],
+
+	init: function(data) {
+		return this.render("timeinput", data);
 	},
   }
 );
